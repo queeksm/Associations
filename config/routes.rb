5be74e5f40e3af7f4f977bfe89f03c1f10eff164
queeksm <queeksm@gmail.com>
2020-06-11 15:44:20 -0500

    Added the user model, and the basic auth feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :events
+  resources :users
+  root 'users#index'
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
