5bb21910edc5cce0a5d333ea8ec70de8777c46d3
Stricks1 <gmalheiross@gmail.com>
2020-06-12 17:53:25 -0300

    linting

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :attended_events
-  has_many :atendees, through: :attended_events, source: :user   
+  has_many :atendees, through: :attended_events, source: :user
 end
