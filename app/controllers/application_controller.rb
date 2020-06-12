79d65f0d8d95a0d1ed736e922aac2cc93fba9f91
Stricks1 <gmalheiross@gmail.com>
2020-06-12 17:51:32 -0300

    linting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
-    
+
   def current_user
     if session[:user_id]
       @current_user ||= User.find(session[:user_id])
--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -17,7 +17,7 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
 
   test 'should create event' do
     assert_difference('Event.count') do
-      post events_url, params: { event: {  } }
+      post events_url, params: { event: {} }
     end
 
     assert_redirected_to event_url(Event.last)
@@ -34,7 +34,7 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should update event' do
-    patch event_url(@event), params: { event: {  } }
+    patch event_url(@event), params: { event: {} }
     assert_redirected_to event_url(@event)
   end
 
